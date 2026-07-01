1c193f0d2267e028d113140ea2b471f7d2f3419f
gohanx8 <gohndavid242@gmail.com>
2026-07-01 21:01:46 +0100

    Add files via upload

---
+++ AppShell.xaml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.IO;
+using Microsoft.Maui;
+using Microsoft.Maui.Controls;
+
+namespace AIDE2
+{
+	public partial class AppShell : Shell
+	{
+		public AppShell()
+		{
+			InitializeComponent();
+			
+		}
+	}
+}
